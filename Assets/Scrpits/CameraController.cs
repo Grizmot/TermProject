826d820d123d8da1041d073653160530a75b3bd3
Jacob <jdank99@gmail.com>
2021-02-21 13:13:57 -0800

    x

---
+++ Assets/Scrpits/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject target;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        transform.position = target.transform.position
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = target.transform.position
+    }
+}
---
+++ Assets/Scrpits/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int speed;
+    Rigidbody2D bod;
+
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)){
+            Jump();
+        }
+
+        if (Input.GetKey(KeyCode.A)){
+            bod.AddForce(Vector2.left * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S)){
+            //bod.AddForce(Vector2.down);
+        }
+
+        if (Input.GetKey(KeyCode.D)){
+            bod.AddForce(Vector2.right * speed);
+        }
+    }
+
+    private void Jump()
+    {
+        if(bod.velocity.y < 0){
+            bod.AddForce(Vector2.up * speed);
+        } 
+    }
+
+}
